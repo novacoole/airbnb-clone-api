c130e9466bdda7374452d049fea4c770bd22f8f6
nova <alexander.coole@protonmail.com>
2020-07-09 11:52:21 +1000

    corrected cors for development

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://funnyname.netlify.app/'
+    origins 'https://funnyname.netlify.app/','http://localhost:3001'
 
     resource '*',
       headers: :any,
