a01fd854e911bbc9bf27bea57e35af684086dccc
nova <alexander.coole@protonmail.com>
2020-07-10 12:54:52 +1000

    changed seed file to make db much smaller

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,20 @@
-# require 'csv'
-# CSV.foreach(Rails.root.join('lib/listings_summary_dec18_reduced.csv'), headers: true) do |row|
-  
-#     Listing.create({
-#       id: row[0],
-#       name: row[1],
-#       neighbourhood: row[2],
-#       latitude: row[3],
-#       longitude: row[4],
-#       room_type: row[5],
-#       price: row[6],
-#       minimum_nights: row[7]
-#     })
-
-# end
+require 'csv'
 require 'open-uri'
+
+CSV.foreach(Rails.root.join('lib/listings_summary_dec18_reduced.csv'), headers: true) do |row|
+  
+    Listing.create({
+      id: row[0],
+      name: row[1],
+      neighbourhood: row[2],
+      latitude: row[3],
+      longitude: row[4],
+      room_type: row[5],
+      price: row[6],
+      minimum_nights: row[7]
+    })
+
+end
 
 Listing.all.each_with_index do |listing,index|
   photo = open('https://source.unsplash.com/400x300/?house')
